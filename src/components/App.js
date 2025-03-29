d9a6d5203b6c91de891108f402317e5cf1b47bed
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-03-29 10:21:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import WeatherDisplay from "./WeatherDisplay";
+import "./../styles/App.css";
 
 const App = () => {
+  // Initialize state for weather data
+  const [weather, setWeather] = useState({ temperature: 0, conditions: "" });
+
+  // useEffect to update weather data on mount
+  useEffect(() => {
+    setWeather({ temperature: 25, conditions: "Sunny" });
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
